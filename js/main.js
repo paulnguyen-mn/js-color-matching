ca59b5103059dd23d7214ee480f98f8221347bcf
Hau Nguyen <paulnguyen.mn@gmail.com>
2020-10-21 22:53:04 +0700

    Add code on class

--- js/main.js
+++ js/main.js
@@ -25,7 +25,132 @@ import { getRandomColorPairs } from './colorHelper.js';
   const playAgainButton = document.querySelector('.game .game__button');
   const colorBackground = document.querySelector('.color-background');
 
-  const init = () => {};
+  const startCountdown = () => {
+    countdownInterval = setInterval(() => {
+      // show timer
+      timerElement.textContent = `${timer}s`;
+
+      // check stop point
+      if (timer <= 0) {
+        gameState = GAME_STATE.FINISHED;
+        timerElement.textContent = 'Game over! 😭';
+        playAgainButton.style.display = 'block';
+
+        clearInterval(countdownInterval);
+      }
+
+      // decrease 1
+      timer -= 1;
+    }, 1000);
+  };
+
+  const reset = () => {
+    // Reset game state
+    selection = [];
+    gameState = GAME_STATE.PENDING;
+    matchCount = 0;
+
+    // Hide play again button
+    playAgainButton.style.display = 'none';
+
+    // Re-generate color list
+    // Bind colors to overlay
+    randomColorList = getRandomColorPairs(PAIRS_COUNT);
+    colorItemList.forEach((liElement, idx) => {
+      liElement.classList.remove('active');
+
+      const color = randomColorList[idx];
+
+      const overlayElement = liElement.querySelector('.overlay');
+      if (overlayElement) {
+        overlayElement.style.backgroundColor = color;
+      }
+    });
+
+    // Reset timer
+    timerElement.textContent = '';
+    timer = GAME_TIME;
+    startCountdown();
+  };
+
+  const handleItemClick = (liElement, idx) => {
+    // Prevent item click
+    if (
+      gameState === GAME_STATE.BLOCKING ||
+      gameState === GAME_STATE.FINISHED ||
+      liElement.classList.contains('active')
+    ) {
+      return;
+    }
+
+    // Show overlay color
+    liElement.classList.add('active');
+
+    // Wait until user select two colors
+    selection.push(idx);
+    if (selection.length < 2) return;
+
+    // Compare selected colors
+    const firstColor = randomColorList[selection[0]];
+    const secondColor = randomColorList[selection[1]];
+    const isMatch = firstColor === secondColor;
+
+    if (!isMatch) {
+      gameState = GAME_STATE.BLOCKING;
+
+      setTimeout(() => {
+        // remove active class from selected liElement
+        colorItemList[selection[0]].classList.remove('active');
+        colorItemList[selection[1]].classList.remove('active');
+
+        // reset selection
+        selection = [];
+
+        gameState = GAME_STATE.PLAYING;
+      }, 500);
+
+      return;
+    }
+
+    // Handle match
+    matchCount += 1;
+    selection = [];
+    colorBackground.style.backgroundColor = firstColor;
+
+    // Check win
+    if (matchCount === PAIRS_COUNT) {
+      timerElement.textContent = 'You win!!! 🎉';
+      playAgainButton.style.display = 'block';
+
+      if (countdownInterval) {
+        clearInterval(countdownInterval);
+      }
+    }
+  };
+
+  const init = () => {
+    // Random colors
+    randomColorList = getRandomColorPairs(PAIRS_COUNT);
+
+    // Bind colors to overlay
+    colorItemList.forEach((liElement, idx) => {
+      const color = randomColorList[idx];
+
+      // Query overlay element and set background color
+      const overlayElement = liElement.querySelector('.overlay');
+      if (overlayElement) {
+        overlayElement.style.backgroundColor = color;
+      }
+
+      liElement.addEventListener('click', () => handleItemClick(liElement, idx));
+    });
+
+    // Bind event click for play again button
+    playAgainButton.addEventListener('click', reset);
+
+    // Start countdown
+    startCountdown(GAME_TIME);
+  };
 
   init();
 })();
