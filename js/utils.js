433e890db0220f543d487da51a208d9ff3020091
minhnva910 <fe.minhnva@gmail.com>
2022-04-12 16:36:15 +0700

    random

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,29 @@
+function suffer(arr) {
+  if (!Array.isArray(arr) || arr.length <= 2) return arr
+  for (let i = arr.length - 1; i > 1; i--) {
+    const j = Math.floor(Math.random() * i)
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+  }
+}
+
+
 export const getRandomColorPairs = (count) => {
   // receive count --> return count * 2 random colors
   // using lib: https://github.com/davidmerfield/randomColor
+
+  const colorList = []
+  const hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']
+
+  for (let i = 0; i < count; i++) {
+    const color = window.randomColor({
+      luminosity: 'light',
+      hue: hueList[i % hueList.length],
+    })
+    colorList.push(color)
+  }
+  const fullColorList = [...colorList, ...colorList]
+  suffer(fullColorList)
+  return fullColorList
 }
